435f80640d11b551871ee1477b09ac72d54d5e21
mdMehediBillah <mailbillah@gmail.com>
2024-10-13 23:44:48 +0200

    Html text added to canvas

--- src/components/ComputerHome.jsx
+++ src/components/ComputerHome.jsx
@@ -1,57 +1,133 @@
 import {
+  CameraControls,
+  Environment,
+  Float,
   Html,
   MeshReflectorMaterial,
-  OrbitControls,
   PerspectiveCamera,
-  Stage,
+  RenderTexture,
+  Text,
 } from "@react-three/drei";
-
 import Computer from "../../public/Computer";
+import { degToRad } from "three/src/math/MathUtils.js";
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import gsap from "gsap";
+// import { currentPageAtom } from "./UI";
 
 const ComputerHome = () => {
+  const controls = useRef();
+  const meshFitCameraHome = useRef();
+  const textRef = useRef();
+  // const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
+
+  useEffect(() => {
+    // Check if textRef.current is available before running the animation
+    if (textRef.current) {
+      // Animate the x, y, and z properties of the position explicitly
+      gsap.fromTo(
+        textRef.current.position,
+        { x: 20, y: 100, z: 0 }, // Starting values
+        {
+          x: -40, // Ending x position
+          y: 0, // Ending y position
+          z: 10, // Ending z position (optional)
+          opacity: 1,
+          duration: 1.5, // Duration of animation
+          ease: "bounce.out", // Bounce ease effect
+          onUpdate: () => {
+            textRef.current.updateMatrix(); // Ensure the object updates its position in the scene
+          },
+        }
+      );
+    }
+  }, []);
+
+  const intro = async () => {
+    controls.current.dolly(-420);
+    controls.current.smoothTime = 1.6;
+    fitCamera();
+  };
+
+  const fitCamera = async () => {
+    controls.current.fitToBox(meshFitCameraHome.current, true);
+  };
+
+  useEffect(() => {
+    intro();
+  }, []);
+
+  useEffect(() => {
+    fitCamera();
+    window.addEventListener("resize", fitCamera);
+    return () => window.removeEventListener("resize", fitCamera);
+  }, []);
+  // currentPage
+
   return (
     <>
-      {" "}
-      <Stage environment="city" intensity={0.6} castShadow>
-        <PerspectiveCamera makeDefault position={[20, 10, 10]} />
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={2}
-          rotateSpeed={0.5}
-          enablePan={false}
-          enableZoom={false}
-          maxAzimuthAngle={Math.PI / 2}
-          maxPolarAngle={Math.PI / 2}
-        />
+      <CameraControls
+        ref={controls}
+        minPolarAngle={Math.PI / 2.8}
+        maxPolarAngle={Math.PI / 2.0}
+        minAzimuthAngle={-Math.PI / 4}
+        maxAzimuthAngle={Math.PI / 4}
+        enableDamping={false}
+        enableZoom={false}
+        autoRotate={true}
+        autoRotateSpeed={1.0}
+      />
+      <mesh position={[0, 80, 0]} ref={meshFitCameraHome} visible={false}>
+        <boxGeometry args={[500, 100, 100]} />
+        <meshBasicMaterial color="orange" transparent opacity={0.5} />
+      </mesh>
 
+      <Text
+        font="fonts/Poppins-Black.ttf"
+        position-x={-120}
+        position-y={-0.5}
+        position-z={-50}
+        lineHeight={0.8}
+        textAlign="center"
+        fontSize={50}
+        rotation-y={degToRad(25)}
+        anchorY={"bottom"}
+        ref={textRef}
+      >
+        WELCOME{"\n"}PORTFOLIO
+        <meshBasicMaterial color="white">
+          <RenderTexture attach="map">
+            <color attach="background" args={["lightgray"]} />
+            <Environment preset="sunset" />
+          </RenderTexture>
+        </meshBasicMaterial>
+      </Text>
+
+      <group rotation-y={degToRad(-25)} position-x={150}>
         <Computer />
-      </Stage>
+      </group>
+
+      <Environment preset="sunset" />
       <mesh
         rotation={[-Math.PI / 2, 0, 0]}
-        position-y={-58}
-        scale={5}
+        position-y={(0, 1)}
+        scale={10}
         receiveShadow
       >
         <planeGeometry args={[170, 170]} />
         <MeshReflectorMaterial
           blur={[300, 100]}
           resolution={2048}
-          mixBlur={5}
-          mixStrength={20}
-          roughness={1}
+          mixBlur={2}
+          mixStrength={8}
+          roughness={8}
           depthScale={1.2}
           minDepthThreshold={0.4}
           maxDepthThreshold={1.4}
           color="#101010"
-          metalness={0.5}
+          metalness={0.3}
         />
       </mesh>
-      <Html className="absolute  py-5 px-10 bg-gray-100 rounded-lg shadow-lg bg-opacity-10 w-[380px]">
-        <h1 className="text-gray-100 font-semibold text-3xl">
-          Hey! This is Mehedi <br />
-          to my Portfolio
-        </h1>
-      </Html>
     </>
   );
 };
--- src/pages/HomeTwo.jsx
+++ src/pages/HomeTwo.jsx
@@ -1,14 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import ComputerHome from "../components/ComputerHome";
-import { Html } from "@react-three/drei";
+import { Float, Html } from "@react-three/drei";
+import { Suspense } from "react";
 
 const HomeTwo = () => {
   return (
     <>
-      <Canvas className="h-screen w-full ">
-        <color attach="background" args={["#101010"]} />
-        <fog attach="fog" args={["#101010", 550, 550]} />
-        <ComputerHome />
+      <Canvas shadows camera={{ position: [0, 200, 800] }}>
+        <color attach="background" args={["#05050f"]} />
+        <fog attach="fog" args={["#101010", 880, 1250]} />
+        <Suspense>
+          <ComputerHome />
+        </Suspense>
+        <Float layoutId="homeBtn" position={[0, -40, 0]}>
+          <Html>
+            <div className="flex flex-col items-center justify-center h-full space-y-4">
+              <h1 className="text-xl font-semibold text-gray-800  bg-rose-500 py-2 px-6 rounded-lg shadow-lg opacity-80 cursor-pointer hover:bg-cyan-500">
+                Enter
+              </h1>
+            </div>
+          </Html>
+        </Float>
       </Canvas>
     </>
   );
